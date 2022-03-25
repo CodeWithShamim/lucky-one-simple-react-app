416164453163f398ba4960470f0268ca690c365f
codewithshamim <shamimislamonline@gmail.com>
2022-03-25 22:01:44 +0600

    cart style added

--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Food.css'
 // font awesome 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 
 const Foods=({food}) => {
     const {img, name, price} = food;
     return (
-        <div className='food-cart'>
-            <img src={img} alt="food-images" />
+        <div className='food-cart col-md-4'>
+            <img src={img} alt="food-images" className='img-fluid' />
             <h3>{name}</h3>
             <h5>{price}</h5>
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import Foods from '../Food/Food';
+import './Shop.css'
 
 const Shop = () => {
     const [foods, setFoods] = useState([]);
@@ -11,11 +12,18 @@ const Shop = () => {
 
     // console.log(foods);
     return (
-        <div className='shop-container'>
-            <div className="food-container">
-                {foods.map(food=><Foods key={food.id} food={food}></Foods>)}
+        <div className="shop-container">
+            <h2>Food House</h2>
+            <div className="row">
+                <div className="food-container col-md-8">
+                    <div className="row  g-4 ">
+                         {foods.map(food=><Foods key={food.id} food={food}></Foods>)}
+                    </div>
+                </div>
+                <div className="cart-container col-md-4">
+                    <h1>Cart container</h1>
+                </div>
             </div>
-            <div className="cart-container"></div>
         </div>
     );
 };
